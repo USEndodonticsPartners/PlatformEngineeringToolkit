beb0462b501874eed027ab0d347d9f28e81315a4
sbarbour1992 <stephen.barbour@adusa.com>
2025-02-04 12:54:57 -0500

    Update global_tags.tf

--- AzureInfrastructure/Standards/global_tags.tf
+++ AzureInfrastructure/Standards/global_tags.tf
@@ -7,7 +7,6 @@ locals {
 
     # Cloud Environment Configurations
     Environment         = var.global_settings.environment
-    Business_Unit       = var.global_settings.business_unit
     Primary_Location    = var.global_settings.primary_location
     Secondary_Location  = var.global_settings.secondary_location == null ? "None" : var.global_settings.secondary_location
     Development_Testing = var.global_settings.environment == "devtst" ? "True" : "False"
