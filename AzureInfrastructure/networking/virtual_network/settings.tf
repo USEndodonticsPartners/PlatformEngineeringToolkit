b7b65961320911e36f04ac5179eeedeebe88739c
sbarbour1992 <stephen.barbour@adusa.com>
2025-02-04 13:35:00 -0500

    Update settings.tf

--- AzureInfrastructure/networking/virtual_network/settings.tf
+++ AzureInfrastructure/networking/virtual_network/settings.tf
@@ -2,7 +2,7 @@ locals {
   env_prefix = lookup(local.environments, var.global_settings.environment, "ndl")
   loc_prefix = lookup(local.prefix_locations, var.global_settings.primary_location, "ndl")
 
-  tags = merge(module.tagging.group_tags, var.global_settings.tags)
+  tags = merge(var.resource_settings.tags, var.global_settings.tags)
 
   # Timeout settings for resource methods
   timeouts = {
