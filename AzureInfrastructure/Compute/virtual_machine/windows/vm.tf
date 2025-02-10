73f21226096a69e7575e718d08fe428059dc2e5e
sbarbour1992 <stephen.barbour@adusa.com>
2025-02-10 14:51:41 -0500

    Update vm.tf

--- AzureInfrastructure/Compute/virtual_machine/windows/vm.tf
+++ AzureInfrastructure/Compute/virtual_machine/windows/vm.tf
@@ -3,8 +3,8 @@ resource "azurerm_windows_virtual_machine" "this" {
   resource_group_name   = var.resource_settings.resource_group_name
   location              = lookup(local.azure_locations, var.global_settings.primary_location, "ndl")
   size                  = var.resource_settings.size
-  admin_username        = var.resource_settings.username
-  admin_password        = ""
+  admin_username        = var.resource_settings.admin_username
+  admin_password        = var.resource_settings.admin_password
   network_interface_ids = var.resource_settings.network_interface_ids
   availability_set_id   = ""
   tags                  = local.tags
