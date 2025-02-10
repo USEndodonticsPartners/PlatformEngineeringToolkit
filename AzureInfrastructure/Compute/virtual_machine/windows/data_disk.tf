df4f8418b1553ce2d809d5fd35e320fdb756f5f6
sbarbour1992 <stephen.barbour@adusa.com>
2025-02-10 14:57:54 -0500

    Update data_disk.tf

--- AzureInfrastructure/Compute/virtual_machine/windows/data_disk.tf
+++ AzureInfrastructure/Compute/virtual_machine/windows/data_disk.tf
@@ -1,5 +1,5 @@
 resource "azurerm_virtual_machine_data_disk_attachment" "this" {
-  for_each = var.resoure_settings.data_disks
+  for_each = var.resource_settings.data_disks
 
   managed_disk_id    = each.value.id
   virtual_machine_id = azurerm_windows_virtual_machine.this.id
