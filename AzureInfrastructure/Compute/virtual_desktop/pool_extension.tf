455533c15673e137261588c1bb00ecd1b1466fb7
sbarbour1992 <stephen.barbour@adusa.com>
2025-02-13 09:58:25 -0500

    update extension

--- AzureInfrastructure/Compute/virtual_desktop/pool_extension.tf
+++ AzureInfrastructure/Compute/virtual_desktop/pool_extension.tf
@@ -1,8 +1,9 @@
 resource "azurerm_virtual_machine_extension" "this" {
+  count    = var.resource_settings.session_pool.count
   for_each = var.resource_settings.extension
 
   name                       = each.value.name
-  virtual_machine_id         = azurerm_windows_virtual_machine.this.id
+  virtual_machine_id         = azurerm_windows_virtual_machine.this[count.index].id
   publisher                  = each.value.publisher
   type                       = each.value.type
   type_handler_version       = each.value.type_handler_version
