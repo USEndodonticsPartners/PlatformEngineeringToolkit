533ec356d6438702e191ee39e76750dedd0c58fd
sbarbour1992 <stephen.barbour@adusa.com>
2025-02-13 10:22:03 -0500

    Update pool_extension.tf

--- AzureInfrastructure/Compute/virtual_desktop/pool_extension.tf
+++ AzureInfrastructure/Compute/virtual_desktop/pool_extension.tf
@@ -1,7 +1,7 @@
 resource "azurerm_virtual_machine_extension" "this" {
   count                      = length(azurerm_windows_virtual_machine.this) * length(keys(var.resource_settings.session_pool.extension))
   name                       = "${element(keys(var.resource_settings.session_pool.extension), count.index % length(var.resource_settings.session_pool.extension))}-extension-${count.index / length(var.resource_settings.session_pool.extension)}"
-  virtual_machine_id         = azurerm_windows_virtual_machine.vm[count.index / length(var.resource_settings.session_pool.extension)].id
+  virtual_machine_id         = azurerm_windows_virtual_machine.this[count.index / length(var.resource_settings.session_pool.extension)].id
   publisher                  = var.resource_settings.session_pool.extension[element(keys(var.resource_settings.session_pool.extension), count.index % length(var.resource_settings.session_pool.extension))].publisher
   type                       = var.resource_settings.session_pool.extension[element(keys(var.resource_settings.session_pool.extension), count.index % length(var.resource_settings.session_pool.extension))].type
   type_handler_version       = var.resource_settings.session_pool.extension[element(keys(var.resource_settings.session_pool.extension), count.index % length(var.resource_settings.session_pool.extension))].type_handler_version
