2c89c55c07aa87d08130b2711c6a3e269c460b08
sbarbour1992 <stephen.barbour@adusa.com>
2025-02-13 10:27:44 -0500

    Update variables.tf

--- AzureInfrastructure/Compute/virtual_desktop/variables.tf
+++ AzureInfrastructure/Compute/virtual_desktop/variables.tf
@@ -41,7 +41,7 @@ variable "resource_settings" {
         sku       = string
         version   = string
       })
-      extension = optional(object({
+      extension = optional(map(object({
         name                 = string
         publisher            = string
         type                 = string
@@ -52,7 +52,7 @@ variable "resource_settings" {
           settings                   = optional(string)
           protected_settings         = optional(string)
         })
-      }))
+      })), {})
     })
     applications = optional(map(object({
       name             = string
