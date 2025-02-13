ea081e43014f6f74ec9ab089b3b7d915bdfa7f35
sbarbour1992 <stephen.barbour@adusa.com>
2025-02-13 10:53:07 -0500

    Update ad_join.tf

--- AzureInfrastructure/Compute/virtual_desktop/ad_join.tf
+++ AzureInfrastructure/Compute/virtual_desktop/ad_join.tf
@@ -8,7 +8,8 @@ resource "azurerm_virtual_machine_extension" "aad_login" {
   type_handler_version = "1.0"
 
   depends_on = [
-    azurerm_virtual_machine_extension.this,
+    azurerm_virtual_machine_extension.integrity_monitor_this,
+    azurerm_virtual_machine_extension.pool_register_this,
     azurerm_windows_virtual_machine.this
   ]
 }
