46d6644e3ba3f9009b43b22e6bca72bc984dff88
sbarbour1992 <stephen.barbour@adusa.com>
2025-02-13 10:20:42 -0500

    Update pool_extension.tf

--- AzureInfrastructure/Compute/virtual_desktop/pool_extension.tf
+++ AzureInfrastructure/Compute/virtual_desktop/pool_extension.tf
@@ -1,13 +1,12 @@
 resource "azurerm_virtual_machine_extension" "this" {
-  for_each                   = var.resource_settings.session_pool.extension
-  count                      = length(azurerm_windows_virtual_machine.this)
-  name                       = var.resource_settings.session_pool.extension.name
-  virtual_machine_id         = azurerm_windows_virtual_machine.this[count.index].id
-  publisher                  = var.resource_settings.session_pool.extension.publisher
-  type                       = var.resource_settings.session_pool.extension.type
-  type_handler_version       = var.resource_settings.session_pool.extension.type_handler_version
-  auto_upgrade_minor_version = lookup(var.resource_settings.session_pool.extension.data, "auto_upgrade_minor_version", null)
-  automatic_upgrade_enabled  = lookup(var.resource_settings.session_pool.extension.data, "automatic_upgrade_enabled", null)
-  settings                   = lookup(var.resource_settings.session_pool.extension.data, "settings", null)
-  protected_settings         = lookup(var.resource_settings.session_pool.extension.data, "protected_settings", null)
+  count                      = length(azurerm_windows_virtual_machine.this) * length(keys(var.resource_settings.session_pool.extension))
+  name                       = "${element(keys(var.resource_settings.session_pool.extension), count.index % length(var.resource_settings.session_pool.extension))}-extension-${count.index / length(var.resource_settings.session_pool.extension)}"
+  virtual_machine_id         = azurerm_windows_virtual_machine.vm[count.index / length(var.resource_settings.session_pool.extension)].id
+  publisher                  = var.resource_settings.session_pool.extension[element(keys(var.resource_settings.session_pool.extension), count.index % length(var.resource_settings.session_pool.extension))].publisher
+  type                       = var.resource_settings.session_pool.extension[element(keys(var.resource_settings.session_pool.extension), count.index % length(var.resource_settings.session_pool.extension))].type
+  type_handler_version       = var.resource_settings.session_pool.extension[element(keys(var.resource_settings.session_pool.extension), count.index % length(var.resource_settings.session_pool.extension))].type_handler_version
+  auto_upgrade_minor_version = var.resource_settings.session_pool.extension[element(keys(var.resource_settings.session_pool.extension), count.index % length(var.resource_settings.session_pool.extension))].auto_upgrade_minor_version
+  automatic_upgrade_enabled  = var.resource_settings.session_pool.extension[element(keys(var.resource_settings.session_pool.extension), count.index % length(var.resource_settings.session_pool.extension))].automatic_upgrade_enabled
+  settings                   = var.resource_settings.session_pool.extension[element(keys(var.resource_settings.session_pool.extension), count.index % length(var.resource_settings.session_pool.extension))].settings
+  protected_settings         = var.resource_settings.session_pool.extension[element(keys(var.resource_settings.session_pool.extension), count.index % length(var.resource_settings.session_pool.extension))].protected_settings
 }
