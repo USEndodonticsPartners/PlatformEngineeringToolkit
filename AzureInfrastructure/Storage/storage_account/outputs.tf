bb00465d0b8a1e9807526c908c4e92c45f715be0
sbarbour1992 <stephen.barbour@adusa.com>
2025-02-13 14:16:52 -0500

    Update outputs.tf

--- AzureInfrastructure/Storage/storage_account/outputs.tf
+++ AzureInfrastructure/Storage/storage_account/outputs.tf
@@ -5,7 +5,3 @@ output "storage_account_id" {
 output "storage_account_uri" {
   value = azurerm_storage_account.this.primary_blob_endpoint
 }
-
-output "storage_account_file_share" {
-  value = azurerm_storage_share.this.id
-}
