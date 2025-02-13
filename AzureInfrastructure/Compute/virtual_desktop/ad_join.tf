132497593946911b0e6fb003fdae7810f6f338eb
sbarbour1992 <stephen.barbour@adusa.com>
2025-02-12 19:57:50 -0500

    Update ad_join.tf

--- AzureInfrastructure/Compute/virtual_desktop/ad_join.tf
+++ AzureInfrastructure/Compute/virtual_desktop/ad_join.tf
@@ -1,21 +1,14 @@
-# resource "azurerm_virtual_machine_extension" "aad_join" {
-#   count                = var.resource_settings.session_host.count
-#   name                 = "AADJoin"
-#   virtual_machine_id   = azurerm_windows_virtual_machine.this[count.index].id
-#   publisher            = "Microsoft.Compute"
-#   type                 = "CustomScriptExtension"
-#   type_handler_version = "1.10"
+resource "azurerm_virtual_machine_extension" "aad_login" {
+  count = var.resource_settings.session_pool.count
 
-#   settings = <<SETTINGS
-#     {
-#       "commandToExecute": "powershell -ExecutionPolicy Unrestricted -File C:\\AzureADJoin.ps1"
-#     }
-#   SETTINGS
+  name                 = "AADLogin"
+  virtual_machine_id   = azurerm_windows_virtual_machine.this[count.index].id
+  publisher            = "Microsoft.Azure.ActiveDirectory"
+  type                 = "AADLoginForWindows"
+  type_handler_version = "1.0"
 
-#   protected_settings = <<PROTECTED_SETTINGS
-#     {
-#       "fileUris": ["https://yourstorageaccount.blob.core.windows.net/avd-scripts/AzureADJoin.ps1?<SAS-Token>"],
-#       "commandToExecute": "powershell -ExecutionPolicy Unrestricted -File AzureADJoin.ps1"
-#     }
-#   PROTECTED_SETTINGS
-# }
+  depends_on = [
+    azurerm_virtual_machine_extension.this,
+    azurerm_windows_virtual_machine.this
+  ]
+}
